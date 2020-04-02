699008599ca15aa6aac6e7a577b9e0ea03e4b69d
Jason Niu <jasonniu@buffalo.edu>
2020-04-02 12:42:38 -0400

    Improved Comments and Testing Print Statements

--- test.py
+++ test.py
@@ -1,12 +1,14 @@
-import numpy as np
 from pyAudioAnalysis import audioTrainTest as aT
 
-# Replace "testing/alaska.wav" with the path of the testing .wav file
-Result, P, classNames = aT.file_classification("testing/alaska.wav", "model", "randomforest")
-winner = np.argmax(P)
+# Replace "testing.wav" with the path of the testing .wav file
+Result, P, classNames = aT.file_classification("testing.wav", "model", "randomforest")
 
-# 0.7 is the desired threshold for classifying an accent (change as desired)
-if P[winner] > 0.7:
-    print("State: " + classNames[winner] + ", with probability: " + str(P[winner]))
-else :
-    print("Can't classify accent: " + str(P))
+# Prints all states with a probability >= 0.2
+states = []
+
+for index in range(len(classNames)):
+    if P[index] >= 0.2:
+        states.append(index)
+
+for state in states:
+    print(classNames[state] + ": " + str(P[state]))
--- train.py
+++ train.py
@@ -1,4 +1,4 @@
 from pyAudioAnalysis import audioTrainTest as aT
 
-# Replace ["Arizona", "Alaska"] with a list of state directories to be trained
+# Replace ["Arizona", "Alaska"] with a list of state directory paths to be trained
 aT.extract_features_and_train(["Arizona","Alaska"], 1.0, 1.0, 0.1, 0.1, "randomforest", "model", False)
