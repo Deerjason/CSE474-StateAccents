1053e5c4e3085d2dd2e3f3bb82304a7a1bc8b118
Deerjason <31902773+Deerjason@users.noreply.github.com>
2020-04-10 11:48:11 -0400

    Bug fix to folder paths

--- train.py
+++ train.py
@@ -8,7 +8,7 @@ from pyAudioAnalysis import audioTrainTest as aT
 #       Arizona 2.wav
 #   > Arkansas
 #       Arkansas 1.wav
-dirs = [directory for directory in os.listdir("train")]
+dirs = ["train/" + directory for directory in os.listdir("train")]
 
 # Trains model using randomforest
 aT.extract_features_and_train(dirs, 1.0, 1.0, 0.1, 0.1, "randomforest", "model", False)
