1a757129ec6a099a8f72e7c3ff3b38372854f282
rishitparihar <54640451+rishitparihar@users.noreply.github.com>
2020-04-11 17:28:26 -0400

    Add files via upload
    Back up one

--- train.py
+++ train.py
@@ -10,44 +10,44 @@
    > Northeast
        > Connecticut
            Connecticut 1.wav
-
 """
 
 import os
 import shutil
 import glob
+from os import listdir
+from os.path import isfile, join
 from pyAudioAnalysis import audioTrainTest as aT
 
+pathtemp = "tempTrain"
+pathOfTrain = "train"
 # Moves all files to temporary directory
-os.mkdir("tempTrain")
-regions = glob.glob("train/*")
+os.mkdir(pathtemp)
+regions = [f.name for f in os.scandir(pathOfTrain) if f.is_dir()]
 for region in regions:
-    states = glob.glob(region + "/*")
-    os.mkdir("tempTrain" + region[region.find("/"):])
+    tempregion = pathOfTrain +"/" + region
+    states = [f.name for f in os.scandir(tempregion) if f.is_dir()]
+    os.mkdir(pathtemp + "/" +region)
     for state in states:
-        paths = glob.glob(state + "/*")
-        for path in paths:
-            shutil.move(path, "tempTrain" + region[region.find("/"):])
-
+        tempstate = join(pathOfTrain,region,state)
+        paths = os.listdir(tempstate)
+        for path in paths:    
+            try:             
+                source = join(pathOfTrain,region,state,path)
+                destination = join(pathtemp,region)
+                shutil.move(source, destination)    
+            except Exception:
+                pass
+            
+            
 # Gets list of directories to be trained on
-dirs = ["tempTrain/" + directory for directory in os.listdir("tempTrain")]
+dirs = [f.path for f in os.scandir(pathtemp) if f.is_dir()]
+
+print(dirs)
 
 # Trains model using randomforest
 aT.extract_features_and_train(dirs, 1.0, 1.0, 0.1, 0.1, "randomforest", "model", False)
 
 # Moves all files back to original directory
-regions = glob.glob("tempTrain/*")
-for region in regions:
-    paths = glob.glob(region + "/*")
-    for path in paths:
-        end = path.find(" ")
-        if not path[end+1:end+2].isdigit():
-            end = path[end+1:].find(" ") + end + 1
-        if end != -1:
-            shutil.move(path, "train" + path[path.find("/"):end])
 
 # Removes temporary directory
-regions = glob.glob("tempTrain/*")
-for region in regions:
-    os.rmdir(region)
-os.rmdir('tempTrain')
