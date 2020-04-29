5e602cc5a4fd873af427118c3cf75348c85fa023
rishitparihar <54640451+rishitparihar@users.noreply.github.com>
2020-04-29 11:06:21 -0400

    Add files via upload

--- tempGUI.py
+++ tempGUI.py
@@ -8,21 +8,23 @@ from pyAudioAnalysis import audioTrainTest as aT
 
 
 root = tk.Tk()
-lableOne = Label(root, text="Waiting for test sample")
+root.geometry("300x100+300+300")
+root.title("Accent Detector")
+lableOne = Label(root, text="",font = "Times 13",anchor = S,width = 20)
 
 def UploadAction(event=None):
-    lableOne.config(text="Processing request")
+    lableOne.config(text="Processing request",font = "Times 13",anchor = S,width = 20)
     source = filedialog.askopenfilename()
     [Result, P, classNames] = aT.file_classification(source, "model", "randomforest")
     if classNames == -1:
-        lableOne.config(text="Error with test sample")
+        lableOne.config(text="Error with test sample",font = "Times 13",anchor = S,width = 20)
     else:
-        lableOne.config(text= classNames[np.argmax(P)])
+        lableOne.config(text= classNames[np.argmax(P)],font = "Times 13",anchor = S,width = 20)
     
 
-button = tk.Button(root,text ="insert pack", fg = "Black", command=UploadAction)
-button.pack()
-lableOne.pack()
+button = tk.Button(root,text ="Insert Audio File", fg = "Black",font = "Times 12", command=UploadAction,width = 20)
+button.place(x=55, y=10)
+lableOne.place(x=55, y=70)
 root.mainloop()
 
 
